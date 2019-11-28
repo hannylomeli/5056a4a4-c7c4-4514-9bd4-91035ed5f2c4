4a85f5195c2ff8a60fe896f125964b7e20322ce1
hannylomeli <hanny_800@hotmail.com>
2019-11-28 07:40:58 -0600

    Scala-intro

--- activities/classwork/scala-intro/src/main/scala/example/c/Main.scala
+++ activities/classwork/scala-intro/src/main/scala/example/c/Main.scala
@@ -2,4 +2,16 @@ package example.c
 
 object Main {
 
+  val numbers: List(Int) = List(1,2,3,4,5)
+
+  def procedure(args: Array[Int]): Box(Int) =
+    numbers.map(num=> Box(num)).foldRight(Box(0)){
+      case (elm,z) => z.concat(elm).map(_.toInt)
+    }
+
+  def main(args: Array[String]): Unit = args match{
+    case valid if args.lenght > 0 => procedure(args.map(_.toInt)).show()
+    case_ => println("Invalid argument.")
+  }
+
 }
--- activities/classwork/scala-intro/src/main/scala/example/d/model/Visitor.scala
+++ activities/classwork/scala-intro/src/main/scala/example/d/model/Visitor.scala
@@ -1,5 +1,27 @@
 package example.d.model
 
-trait Visitor {
+import java.sql.Timestamp
 
+sealed trait Visitor {
+  def id:String
+  def createdAt: Timestamp
+
+  def getAgeInSeconds: Int = createdAt.getSeconds
+
+  def show() : Unit = this match {
+    case Visitor.Anonymous(id, createdAt) => printLn (s"Anonymous user with id $id")
+    case Visitor.User(_, email, _) =>
+      println(s"User with email $email")
+  }
+
+  def getEmail: Option[String]
+}
+
+object Visitor {
+  final case class Anonymous(id:String, created: Timestamp) extends Visitor {
+    override def getEmail: Option[String] = None
+  }
+  final case class User(id:String, email: String, created: Timestamp) extends Visitor {
+    override def getEmail: Option[String] = Some(email)
+  }
 }
